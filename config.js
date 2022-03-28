b00de8b7d232e773e6fafca9273056d51aa0ebbe
Jackky46 <64960132+jackky46@users.noreply.github.com>
2022-03-28 15:16:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@
 
 require("dotenv").config();
 
-const client_id = process.env.clientID || 'fb7cad0b25154d45b035d6918a70de03';
+const client_id = process.env.clientID || '';
 const redirect_uri = 'http://localhost:3000/callback';
-const client_secret = process.env.clientSecret || '2514c4b30cf048dcb766c501b1f10cd1';
+const client_secret = process.env.clientSecret || '';
